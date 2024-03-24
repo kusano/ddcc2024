bd8212a7c1b3d75c61c18d1b74df00f239e62654
kusano <kusano@users.noreply.github.com>
2024-03-24 15:16:29 +0900

    ファイナル
    24/03/24 15:16:09
    
    6776.88
    6776.88

--- Q2.py
+++ Q2.py
@@ -12,7 +12,8 @@ for i in range(4):
 
 g = 9.8*1000
 
-for t in [0, 3, 2, 5, 1, 6]:
+Ts = [0, 3, 2, 5, 1, 6]
+for t in Ts:
     Th = t+1
     Ch = 0
     Lh = 0
@@ -26,7 +27,7 @@ for t in [0, 3, 2, 5, 1, 6]:
     sr = [(tr_tmp+(0 if t==0 else 90000))%180000]*4
     et = math.hypot(HX[t], HY[t])/500
 
-    if t in [2, 5, 1, 6]:
+    if t in [2, 5, 1, 6, 4]:
         if t in [2, 5]:
             tr = -math.pi/4
         else:
@@ -35,28 +36,39 @@ for t in [0, 3, 2, 5, 1, 6]:
         vx = v*math.sin(tr)
         vy = v*math.cos(tr)
         et = HY[t]/vy
+        if t in [5, 6]:
+            et *= 1.1
         best = 1e10
         for a in range(-10000,10000):
+        #for a in range(-10,10):
             if a==0:
                 continue
             a = a/100000
+            #a /= 100
             #t2 = -vx/((1/2)*(5/7)*g*math.sin(a)*math.cos(a))
             fa = (1/2)*(5/7)*g*math.sin(a)*math.cos(a)
             fb = vx
             fc = -HX[t]
-            t2 = (-fb+(fb**2-4*fa*fc)**.5)/(2*fa)
+            sign = 1 if a>0 else -1
+            t2 = (-fb+sign*(fb**2-4*fa*fc)**.5)/(2*fa)
             #print(a, a/math.pi*180, abs(t2-et))
             if abs(t2-et)<best:
                 best = abs(t2-et)
                 besta = a
-        #print("besta:", besta/math.pi*180)
+        #print("besta:", besta/math.pi*180, HX[t])
         d = 800*math.sin(besta)
         Lh = min(25000, max(-25000, int(1000*d/2)))
         Rh = min(25000, max(-25000, -int(1000*d/2)))
         tr = int(tr/math.pi*180*1000)
 
+        if t==6:
+            sr[0:3] = [0]*3
+
     Nu = 10
     Wt = int(et*2*1000)
-    #Wt = int((55.-(2+.5*10)*4)/3*1000)
+    if t==5:
+        Wt -= 1000
+    if t==Ts[-1]:
+        Wt = 10000
 
     print(f"{Th},{Ch},{Lh},{Rh},{tr},{sr[0]},{sr[1]},{sr[2]},{sr[3]},{Nu},{Wt}")
