49965831ef89b8e80b09a4d1ab46131152c81274
kusano <kusano@users.noreply.github.com>
2024-03-24 13:15:51 +0900

    傾きを使う
    8537.68
    8537.68

--- Q2.py
+++ Q2.py
@@ -10,7 +10,9 @@ SY = [0]*7
 for i in range(4):
     SX[i], SY[i] = q[14+2*i:14+2*i+2]
 
-for t in [0, 3, 1, 2]:
+g = 9.8*1000
+
+for t in [0, 3, 2, 5, 1]:
     Th = t+1
     Ch = 0
     Lh = 0
@@ -21,12 +23,36 @@ for t in [0, 3, 1, 2]:
         Rh = -25000
     tr_tmp = int(math.atan2(HX[t], HY[t])/(2*math.pi)*360*1000)
     tr = max(-45000, min(45000, tr_tmp))
-    sr = [(tr_tmp+(0 if t==0 else 9000))%18000]*4
-    if t==2:
-        d = 25000
-        Lh = d
-        Rh = -d
-        tr = -45000
+    sr = [(tr_tmp+(0 if t==0 else 90000))%180000]*4
+    et = math.hypot(HX[t], HY[t])/500
+
+    if t in [2, 5]:
+        tr = -math.pi/4
+        v = 500 # mm/s
+        vx = v*math.sin(tr)
+        vy = v*math.cos(tr)
+        et = HY[t]/vy
+        best = 1e10
+        for a in range(1,10000):
+            a = a/100000
+            t2 = -vx/((1/2)*(5/7)*g*math.sin(a)*math.cos(a))
+            #print(a, a/math.pi*180, abs(t2-et))
+            if abs(t2-et)<best:
+                best = abs(t2-et)
+                besta = a
+        #print("besta:", besta/math.pi*180)
+        d = 800*math.sin(besta)
+        Lh = int(1000*d/2)
+        Rh = -int(1000*d/2)
+        tr = int(tr/math.pi*180*1000)
+
+    if t==1:
+        Lh = 25000
+        Rh = -25000
+        tr = 0
+
     Nu = 10
-    Wt = int((55.-(2+.5*10)*4)/3*1000)
+    Wt = int(et*2*1000)
+    #Wt = int((55.-(2+.5*10)*4)/3*1000)
+
     print(f"{Th},{Ch},{Lh},{Rh},{tr},{sr[0]},{sr[1]},{sr[2]},{sr[3]},{Nu},{Wt}{';'if t==6 else''}")
